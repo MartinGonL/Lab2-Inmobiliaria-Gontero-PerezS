b11e6e6cacd161cdbc8a6ef31884b9fa127e0287
SimonPerez21 <simoonperez04@gmail.com>
2025-09-30 15:06:50 -0300

    views

--- Controllers/PagosController.cs
+++ Controllers/PagosController.cs
@@ -1,80 +1,93 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Inmobiliaria_.Net_Core.Models;
 
-namespace Inmobiliaria_.Net_Core.Controllers
+public class PagosController : Controller
 {
-    public class PagosController : Controller
+    private readonly RepositorioPago repoPago;
+    private readonly RepositorioContrato repoContrato;
+
+    public PagosController(IConfiguration config)
     {
-        private readonly IRepositorioPago repo;
+        repoPago = new RepositorioPago(config);
+        repoContrato = new RepositorioContrato(config);
+    }
 
-        public PagosController(IRepositorioPago repo)
-        {
-            this.repo = repo;
-        }
+    // GET: Pagos
+    public IActionResult Index()
+    {
+        var lista = repoPago.ObtenerTodos();
+        return View(lista);
+    }
 
-        public IActionResult Index()
-        {
-            var lista = repo.ObtenerTodos();
-            return View(lista);
-        }
+    // GET: Pagos/Details/5
+    public IActionResult Details(int id)
+    {
+        var pago = repoPago.ObtenerPorId(id);
+        if (pago == null) return NotFound();
+        return View(pago);
+    }
 
-        public IActionResult Details(int id)
-        {
-            var pago = repo.ObtenerPorId(id);
-            if (pago == null) return NotFound();
-            return View(pago);
-        }
+    // GET: Pagos/Create
+    public IActionResult Create()
+    {
+        ViewBag.Contratos = new SelectList(repoContrato.ObtenerTodos(), "IdContrato", "Descripcion");
+        return View();
+    }
 
-        public IActionResult Create()
+    // POST: Pagos/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Pago pago)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Pago p)
-        {
-            if (ModelState.IsValid)
-            {
-                repo.Alta(p);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(p);
-        }
-
-        public IActionResult Edit(int id)
-        {
-            var pago = repo.ObtenerPorId(id);
-            if (pago == null) return NotFound();
-            return View(pago);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Pago p)
-        {
-            if (id != p.Id_Pago) return BadRequest();
-            if (ModelState.IsValid)
-            {
-                repo.Modificacion(p);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(p);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            var pago = repo.ObtenerPorId(id);
-            if (pago == null) return NotFound();
-            return View(pago);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            repo.Baja(id);
+            repoPago.Alta(pago);
             return RedirectToAction(nameof(Index));
         }
+        ViewBag.Contratos = new SelectList(repoContrato.ObtenerTodos(), "IdContrato", "Descripcion", pago.IdContrato);
+        return View(pago);
+    }
+
+    // GET: Pagos/Edit/5
+    public IActionResult Edit(int id)
+    {
+        var pago = repoPago.ObtenerPorId(id);
+        if (pago == null) return NotFound();
+        ViewBag.Contratos = new SelectList(repoContrato.ObtenerTodos(), "IdContrato", "Descripcion", pago.IdContrato);
+        return View(pago);
+    }
+
+    // POST: Pagos/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Pago pago)
+    {
+        if (id != pago.IdPago) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            repoPago.Modificacion(pago);
+            return RedirectToAction(nameof(Index));
+        }
+        ViewBag.Contratos = new SelectList(repoContrato.ObtenerTodos(), "IdContrato", "Descripcion", pago.IdContrato);
+        return View(pago);
+    }
+
+    // GET: Pagos/Delete/5
+    public IActionResult Delete(int id)
+    {
+        var pago = repoPago.ObtenerPorId(id);
+        if (pago == null) return NotFound();
+        return View(pago);
+    }
+
+    // POST: Pagos/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        repoPago.Baja(id);
+        return RedirectToAction(nameof(Index));
     }
 }
